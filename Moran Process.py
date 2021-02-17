bee4aa655dd2e0575aee9eac5c1c6625414457db
Joac1137 <Joachim@brendborg.dk>
2021-02-17 13:57:19 +0100

    Started drawing graphs and understanding networkx library. Further started making a TODO list with thing to implement. Further it should help as a guideline as to how to project is to be structured

--- Moran Process.py
+++ Moran Process.py
@@ -1,9 +1,37 @@
-
-
-
+import networkx as nx
+import matplotlib.pyplot as plt
 
 
 
 
 if __name__ == "__main__":
-    print("hello")
+   G = nx.Graph()
+   G.add_node(1)
+
+   G.add_nodes_from([2,3])
+   G.add_nodes_from([
+       (4, {"color": "red"}),
+       (5, {"color": "green"}),
+   ])
+
+   G.add_edge(1,2)
+   e = (2,3)
+   G.add_edge(*e)
+
+   G.add_edges_from([(1,2),(1,3)])
+
+   print(list(G.nodes))
+
+
+   H = nx.karate_club_graph()
+   print(H)
+   print(H.nodes[5]["club"])
+
+   print(H.nodes[9]["club"])
+
+   nx.draw_circular(H, with_labels=True)
+   plt.show()
+
+
+
+
